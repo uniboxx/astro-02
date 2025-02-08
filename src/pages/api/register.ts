56b9c843663cdc317472964f705be94541b333ab
unibox <unibox@duck.com>
2025-02-08 13:11:30 +0100

    add users table and add user

---
+++ src/pages/api/register.ts
@@ -0,0 +1,26 @@
+export const prerender = false;
+import type { APIRoute } from 'astro';
+import { turso } from 'turso';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+export const POST: APIRoute = async function ({ request, redirect }) {
+  console.log(request);
+  const data = Object.fromEntries(await request.formData());
+  const { username, email, password } = data;
+  bcrypt.hash(password.toString(), saltRounds, async function (err, hash) {
+    if (err) {
+      throw new Error('Something went wrong hashing password');
+    }
+    try {
+      await turso.execute({
+        sql: 'INSERT INTO users (username,email,password) VALUES (?, ?, ?)',
+        args: [username.toString(), email.toString(), hash.toString()],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  return redirect('/');
+};
---
+++ src/utils/hash-generator.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+export function createHash(plainPassword: string) {
+  bcrypt.hash(plainPassword, saltRounds, function (err, hash) {
+    if (err) {
+      throw new Error('Something went wrong hashing password');
+    }
+    console.log(hash);
+    compareHash('password', hash);
+  });
+}
+
+export function compareHash(plainPassword: string, hash: string) {
+  return bcrypt.compare(plainPassword, hash, function (err, result) {
+    if (err) {
+      throw new Error('Something went wrong checking password');
+    }
+    console.log(result);
+  });
+}
+
+createHash('password');
