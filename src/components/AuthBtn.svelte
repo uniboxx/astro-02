2024b3deeae75dd058eb061e1159ea18d82ab1ed
unibox <unibox@duck.com>
2025-02-10 17:10:57 +0100

    add button signin mobile

--- src/components/AuthBtn.svelte
+++ src/components/AuthBtn.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { signinWithGithub, signinWithGoogle } from '@utils/auth-client';
-  const { mode, logo } = $props();
+
+  const { mode, logo, disabled } = $props();
   let text =
     mode[0].toUpperCase() +
     mode.slice(1, 6) +
@@ -18,6 +19,7 @@
   class="flex items-center gap-2 px-4 py-2 rounded cursor-pointer md:border-gray-300 md:border-1"
   id={mode}
   onclick={signIn}
+  {disabled}
 >
   <img class="w-4 md:w-5" src={logo.src} alt={`${text.slice(10)} logo`} />
   <span class="text-sm">{text}</span>
